e677ebfdcb001fb7de056d4cc6041c1ae85f221b
EversonBorges <everson.cursos@gmail.com>
2021-10-16 12:54:17 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Highlight from './highlight';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [word, setWord] = useState('');
+    function handleChange(event){
+        setWord(event.target.value)
+    }
+
+    return (
+        <>
+            <input type='text' value={word} onChange={handleChange}/>
+            <p><Highlight children='Texto a ser marcado' toHighlight={word}/></p>
+            <p><Highlight children='Um outro texto que também poderá ser marcado' toHighlight={word}/></p>
+        
+            <table>
+                <thead>
+                    <tr>
+                        <th>Cursos</th>
+                        <th>Horas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                        <Highlight children='Javascript Ninja' toHighlight={word}/></td>
+                        <td>42h</td>
+                    </tr>
+                    <tr>
+                        <td>React Ninja</td>
+                        <td>60h</td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
 }
-
 export default App;
